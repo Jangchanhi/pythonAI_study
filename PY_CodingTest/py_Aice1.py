3e2096e4388e2e2f019fb28690c58b81b0fa14db
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-03-14 20:38:46 +0900

    3) append : 4) insert : 5) extend : 6) remove :

--- PY_CodingTest/py_Aice1.py
+++ PY_CodingTest/py_Aice1.py
@@ -12,10 +12,39 @@ print(x[0:4])
 print(x[12:])
 print(x[:7])
 
+# python 대표 자료형 String(문자열), integer(정수형), float(소수점 자리)
+# List 자료형 : 순서대로 정리된 항목들을 담는 구조(특정 데이터를 반복적으로 처리하는데 특화)
+a = [1,2,3] # 리스트 자료형
+b = list(range(1,10,2))
 
+# Tuple 자료형 :  ()으로 둘러쌈 a=(1,2,3), b = (2,)
+# Pandas 자료형
+print(a)
 
+c = ['red', 'green', 'blue']
+c.append('yellow') # 리스트의 끝에 yellow라는 요소 추가
+print(c)
 
+c.insert(1, 'black') # 'black'이라는 요소를 1번째 자리에 넣는다.
+print(c)
 
+d = ['purple', 'white']
+c.extend(d) # d의 요소들을 c에 넣는다.
+print(c)
+
+q = [10, 20, 30, 40,50,60,70,80,90,100]
+q.remove(90) # 90이라는 요소 삭제
+print(q)
+
+list1 = ['a','bb','c','d','aaa','c','ddd','aaa','b','cc','d','aaa',]
+print(list1.count('aaa')) # aaa라는 것의 개수를 알 수 있다.
+
+list2 = [1,-7,5,8,3,9,11,13]
+list2.sort()
+print(list2)
+
+list2.sort(reverse=True)
+print(list2)
 
 
 

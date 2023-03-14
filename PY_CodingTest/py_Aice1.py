9d3277d9369376aa462d835ec47c77983c6c74a3
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-03-14 19:28:48 +0900

    AICE 강의를 듣고 실습한 것 1) 인덱싱 : 특정한 값을 뽑아내는 것, 무엇인가를 가리킬 때 2) 슬라이싱 : 원하는 단어를 뽑아내고자 할때

---
+++ PY_CodingTest/py_Aice1.py
@@ -0,0 +1,21 @@
+# Indexing : 무엇인가 가르킨다를 의미
+# 인덱싱-> 특정한 값을 뽑아내는 역할
+x = "Rome is not built in a day!"
+print(x[0])
+print(x[-1])
+print(x[-6])
+print(x[12])
+# 슬라이싱(Slicing) : 원하는 단어를 뽑아내고자 할 때 사용
+# x[시작 번호 : 끝번호]
+print("슬라이싱 연습")
+print(x[0:4])
+print(x[12:])
+print(x[:7])
+
+
+
+
+
+
+
+

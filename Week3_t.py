2120247f6119c580521d7a906de638361e66a798
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-03-23 23:30:50 +0900

    머신러닝 3주차 테스트

--- Week3_t.py
+++ Week3_t.py
@@ -36,5 +36,62 @@ plt.show()
 print(np.mean(train_target[indexes]))
 
 print(knr.predict([[100]]))
+distances, indexes = knr.kneighbors([[100]])
+plt.scatter(train_input, train_target)
+plt.scatter(train_input[indexes], train_target[indexes], marker='D')
+plt.scatter(100, 1033, marker='^')
+plt.show()
+
+lr = LinearRegression()
+lr.fit(train_input, train_target)
+print(lr.predict([[50]]))
+print(lr.coef_, lr.intercept_)
+plt.scatter(train_input, train_target)
+#plt.plot([15, 50], [15*lr.coef_+lr.intercept_, 50*lr.coef_+lr.intercept_])
+plt.scatter(50, 1241.8, marker='^')
+plt.show()
+print(lr.score(train_input, train_target))
+print(lr.score(test_input, test_target))
+
+train_poly = np.column_stack((train_input ** 2, train_input))
+test_poly = np.column_stack((test_input ** 2, test_input))
+
+#%%
+
+print(train_poly.shape, test_poly.shape)
+
+#%%
+
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+
+print(lr.predict([[50**2, 50]]))
+
+#%%
+
+print(lr.coef_, lr.intercept_)
+
+#%%
+
+# 구간별 직선을 그리기 위해 15에서 49까지 정수 배열을 만듭니다
+point = np.arange(15, 50)
+# 훈련 세트의 산점도를 그립니다
+plt.scatter(train_input, train_target)
+# 15에서 49까지 2차 방정식 그래프를 그립니다
+plt.plot(point, 1.01*point**2 - 21.6*point + 116.05)
+# 50cm 농어 데이터
+plt.scatter([50], [1574], marker='^')
+plt.show()
+
+#%%
+
+print(lr.score(train_poly, train_target))
+print(lr.score(test_poly, test_target))
+
+plt.xlabel('length')
+plt.ylabel('weight')
+
+
+
 
 

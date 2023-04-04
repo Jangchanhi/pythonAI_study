2165c64be66d7363f4e9abf59a087ebd287a01d0
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-04 23:55:49 +0900

    머신러닝 5주차 정리 및 복습 결정 트리

--- Week5_t.py
+++ Week5_t.py
@@ -32,17 +32,17 @@ print(lr.score(test_scaled, test_target))
 
 print(lr.coef_, lr.intercept_)
 
-# # 결정 트리 방법 사용
-# dt = DecisionTreeClassifier(random_state=42)
-# dt.fit(train_scaled, train_target)
-# # Tree : 특정 변수를 임계값을 구분하여 오차를 최소화하는 N-중 if문을 구성해줌
-#
-# print(dt.score(train_scaled, train_target))
-# print(dt.score(test_scaled, test_target))
-#
-# plt.figure(figsize=(10,7))
-# plot_tree(dt, max_depth=1, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
-# plt.show()
+# 결정 트리 방법 사용
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(train_scaled, train_target)
+# Tree : 특정 변수를 임계값을 구분하여 오차를 최소화하는 N-중 if문을 구성해줌
+
+print(dt.score(train_scaled, train_target))
+print(dt.score(test_scaled, test_target))
+
+plt.figure(figsize=(10,7))
+plot_tree(dt, max_depth=1, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
+plt.show()
 
 # GINI 불순도가 0.5 면 계속 분류를 해야 하고 지니 불순도가 0이 되면 분류가 다 되었다는 순수노드가 되었다는 뜻이다.
 

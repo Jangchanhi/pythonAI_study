39d7aab730b7bb7009ae44d5f723b21c184f999d
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-10 23:47:41 +0900

    머신러닝 5주차 실습 복습

--- Week5_w.py
+++ Week5_w.py
@@ -38,3 +38,59 @@ axs[0].imshow(apple_mean, cmap='gray_r')
 axs[1].imshow(pineapple_mean, cmap='gray_r')
 axs[2].imshow(banana_mean, cmap='gray_r')
 plt.show()
+
+# 평균과 가까운 사진 고르기
+# 비슷한 것을 빼면 값이 작아진다.
+
+# 각 과일과 고르고자 하는 과일 평균 값의 차이
+abs_diff = np.abs(fruits - banana_mean)
+# 차이를 평균
+abs_mean = np.mean(abs_diff, axis=(1, 2))
+print(abs_mean.shape)
+# 차이가 적은 상위 100개만 뽑음
+apple_index = np.argsort(abs_mean)[:100]
+fig, axs = plt.subplots(10, 10, figsize=(10, 10))
+for i in range(10):
+    for j in range(10):
+        axs[i, j].imshow(fruits[apple_index[i * 10 + j]], cmap='gray_r')
+        axs[i, j].axis('off')
+
+# 고른 과일 평균 값과 가장 유사한 이미지부터 순서대로 출력
+plt.show()
+
+# 군집 (Cluster)
+# 무엇인지 모르지만(비지도학습) 사전에 정해진 특징들울 기준으로 K개의 군집으로 분류함
+
+# k-평균
+# K-Means 알고리즘
+# 1. 임의로 K개의 중심점을 구함
+# 2. 각 중심점에서 가장 가까운 샘플들을 찾아냄 -> K 개의 군집이 형성
+# 3. 각 군집의 값들의 평균을 구해 새로운 K개의 중심점을 구함
+# 4. 클러스터 중심의 변화가 일정 값 이하가 될때 까지 2~3을 반복함
+
+km = KMeans(n_clusters=3, random_state=42)
+fruits_2d = fruits.reshape(-1, 100 * 100)
+km.fit(fruits_2d)
+
+print(km.labels_)
+print(np.unique(km.labels_, return_counts=True))
+
+
+def draw_fruits(arr, ratio=1):
+    n = len(arr)
+    rows = int(np.ceil(n / 10))
+    cols = n if rows < 2 else 10
+    fig, axs = plt.subplots(rows, cols, figsize=(cols * ratio, rows * ratio), squeeze=False)
+
+    for i in range(rows):
+        for j in range(cols):
+            if i * 10 + j < n:
+                axs[i, j].imshow(arr[i * 10 + j], cmap='gray_r')
+            axs[i, j].axis('off')
+    plt.show()
+
+
+draw_fruits(fruits[km.labels_ == 0])
+
+
+

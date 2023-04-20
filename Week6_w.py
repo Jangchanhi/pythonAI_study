f3b5274cb1aac97a6460c5927cb74d642e15e7ae
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-20 23:45:33 +0900

    머신러닝 실습 복습

--- Week6_w.py
+++ Week6_w.py
@@ -33,13 +33,13 @@ print(np.mean(scores['test_score']))
 # train_scaled, val_scaled, train_target, val_target = train_test_split(train_scaled, train_target, test_size= = 0.2, random_state=42)
 
 print(train_scaled.shape, train_target.shape)
-#
+
 # print(val_scaled.shape, val_target.shape)
-# dense = keras.layers.Dense(10, activation='softmax', input_shape=(784,))
-# model = keras.Sequential(dense)
-#
-# model.compile(loss='sparse_categorical_crossentropy',metrics='accuracy')
-# print(train_target[:10])
+dense = keras.layers.Dense(10, activation='softmax', input_shape=(784,))
+model = keras.Sequential(dense)
+
+model.compile(loss='sparse_categorical_crossentropy',metrics='accuracy')
+print(train_target[:10])
 
 
 

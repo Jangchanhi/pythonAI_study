a33dfea30640af5e0045db079321505d8d17962c
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-22 23:22:22 +0900

    머신러닝 실습 복습

--- Week5_homework.py
+++ Week5_homework.py
@@ -60,19 +60,19 @@ plot_tree(dt, max_depth=1, filled=True, feature_names=['alcohol', 'sugar', 'pH']
 plt.show()
 
 # GINI 불순도가 0.5 면 계속 분류를 해야 하고 지니 불순도가 0이 되면 분류가 다 되었다는 순수노드가 되었다는 뜻이다.
-#
-# # 가지치기 방법
-# dt = DecisionTreeClassifier(max_depth=3, random_state=42)
-# dt.fit(train_scaled, train_target)
-# # Tree : 특정 변수를 임계값을 구분하여 오차를 최소화하는 N-중 if문을 구성해줌
-#
-# print(dt.score(train_scaled, train_target))
-# print(dt.score(test_scaled, test_target))
-#
-# plt.figure(figsize=(20, 15))
-# plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
-# plt.show()
-#
+
+# 가지치기 방법
+dt = DecisionTreeClassifier(max_depth=3, random_state=42)
+dt.fit(train_scaled, train_target)
+# Tree : 특정 변수를 임계값을 구분하여 오차를 최소화하는 N-중 if문을 구성해줌
+
+print(dt.score(train_scaled, train_target))
+print(dt.score(test_scaled, test_target))
+
+plt.figure(figsize=(20, 15))
+plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
+plt.show()
+
 
 
 

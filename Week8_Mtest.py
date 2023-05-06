79343489b46eb186d898f0c2189ad20aac74e925
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-06 23:11:48 +0900

    중간 테스트 코드

---
+++ Week8_Mtest.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from scipy.special import softmax
+
+
+
+data = pd.read_csv('midterm.csv')
+
+data_input = data[['Weight','Length','Height','Width']].to_numpy()
+data_target = data['Species'].to_numpy()
+
+train_input, test_input , train_target, test_target=train_test_split(data_input, data_target, random_state=42)
+
+ss = StandardScaler()
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+lr = LogisticRegression()
+lr.fit(train_scaled,train_target)
+decision = lr.decision_function(test_scaled)
+print(np.round(decision, decimals=2))
+
+proba = softmax(decision,axis=1)
+print(np.round(proba,decimals=3))
+
+
+
+
